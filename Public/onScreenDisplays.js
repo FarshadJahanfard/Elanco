dd2425af12957c475b8f8c1654f12e15095afa77
Martell Simpson <c1022557@hallam.shu.ac.uk>
2024-02-22 10:21:18 +0000

    Neatened files

---
+++ Public/onScreenDisplays.js
@@ -0,0 +1,48 @@
+function fetchDogData(dogId) {
+    const url = `http://localhost:3001/api/temperature/dog/${dogId}`;
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Since your data is an array of objects, access the first item and then its 'Temperature (C)' property
+            const temperature = data[0]["Temperature (C)"]; // Adjust based on actual data structure
+    
+            // Update the text in the middle of the chart
+            document.getElementById('chartCenterText').textContent = `${temperature}°C`;
+        })
+        .catch(error => {
+            console.error('11111 tching temperature data:', error);
+            document.getElementById('chartCenterText').textContent = 'Error';
+        });
+}
+function fetchDogData1(dogId) {
+    const url = `http://localhost:3001/api/weight/dog/${dogId}`;
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Since your data is an array of objects, access the first item and then its 'Temperature (C)' property
+            const weight = data[0]["Weight (kg)"]; // Adjust based on actual data structure
+    
+            // Update the text in the middle of the chart
+            document.getElementById('chartCenterText1').textContent = `${weight} kg`;
+        })
+        .catch(error => {
+            console.error('Error weight data:', error);
+            document.getElementById('chartCenterText1').textContent = 'Error';
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Call the function with the specific dog ID
+    fetchDogData('CANINE001'); 
+    fetchDogData1('CANINE001');// Replace 'CANINE001' with the actual dog ID you're interested in
+});
