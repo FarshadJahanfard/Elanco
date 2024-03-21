0e26c264b2bcca008fb52df1123fd153d30b826b
hassan755-bit <hassanshab98@icloud.com>
2024-03-21 20:11:44 +0000

    added food all time average

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const averageFoodIntake = require("./routes/averagefoodIntake.js");
 const yearlyAverageWeight = require("./routes/yearlyAverageWeight.js");
 const yearlyAverageTemperature = require("./routes/yearlyAverageTemperature.js");
 const yearlyBreathingRate = require("./routes/yearlyBreathingRate.js");
+const yearlyFoodIntake = require("./routes/yearlyFoodIntake.js");
 
 app.get("/", (req, res) => res.redirect("/login.html"));
 
@@ -68,6 +69,7 @@ async function main() {
     app.use("/api/yearlyAverageWeight", yearlyAverageWeight);
     app.use("/api/yearlyAverageTemperature", yearlyAverageTemperature);
     app.use("/api/yearlyBreathingRate", yearlyBreathingRate);
+    app.use("/api/yearlyFoodIntake", yearlyFoodIntake);
     // Start the server
     app.listen(process.env.PORT, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
